6250ca762f6ff93d51c1bd9865c4a30e8ce4b309
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-02 11:38:30 -0300

    method list category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,19 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
+  validates :name, presence: true, uniqueness: true
+  belongs_to :category
+  def sub_categories
+    Category.all.where(category_id: self.id).pluck :name
+  end
+  
+  def self.list_categories
+    list = {}
+    
+    Category.all.each do |d|
+      if d.sub_categories
+          list[ d.name] = d.sub_categories
+      end
+    end
+    list
+  end
 end
