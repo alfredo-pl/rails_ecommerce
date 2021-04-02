5bef31d3f050974e97bb63800245e7b8c237c0d4
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-02 11:32:00 -0300

    stock product

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @products = Product.all
+    @products = Product.stock_product
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,15 @@ class Product < ApplicationRecord
 
   has_many :order_items
   has_many :orders, through: :order_items
+  has_many :variations, dependent: :destroy
+
+  def self.stock_product
+    stock_product = []
+    self.all.each do |product|
+      if product.variations.first && product.variations.first.stock > 0
+        stock_product << product
+      end
+    end
+    stock_product
+  end
 end
