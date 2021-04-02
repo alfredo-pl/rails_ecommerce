8ffd6b83710da3ea0f1a4ce212d5533e91f2152a
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-02 11:36:57 -0300

    list product variations

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -2,4 +2,9 @@ class Variation < ApplicationRecord
   belongs_to :product
   belongs_to :color
   belongs_to :size
+
+  def self.product_list
+    x = Variation.select(:product_id).distinct.pluck(:product_id)
+    Product.where(id: x)
+  end
 end
