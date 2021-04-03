9efe0c13f4642404de7a86182b037bcaa0c97abd
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-03 11:14:31 -0300

    delete method

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -3,8 +3,4 @@ class Variation < ApplicationRecord
   belongs_to :color
   belongs_to :size
 
-  def self.product_list
-    x = Variation.select(:product_id).distinct.pluck(:product_id)
-    Product.where(id: x)
-  end
 end
