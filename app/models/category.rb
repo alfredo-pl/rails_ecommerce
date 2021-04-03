eac2f91707f6201caca014ce857c8539c650037f
Alfredo Peña <aalfredoplz@gmail.com>
2021-04-03 11:07:22 -0300

    has many

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
+  has_many :child_categories, class_name: "Category", foreign_key: "category_id"
+  belongs_to :parent_category, class_name: "Category", optional: true, foreign_key: 'category_id'
   validates :name, presence: true, uniqueness: true
- 
+
   def sub_categories
     Category.all.where(category_id: self.id).pluck :name
   end
